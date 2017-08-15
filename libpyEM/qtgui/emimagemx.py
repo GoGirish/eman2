201d3d4572cb0877a40dea5a07ce6eede4fa41fc
shadow_walker <shadowwalkersb@gmail.com>
2017-08-15 01:01:18 -0500

    bool -> flag

--- libpyEM/qtgui/emimagemx.py
+++ libpyEM/qtgui/emimagemx.py
@@ -639,12 +639,12 @@ class EMImageMXWidget(EMGLWidget, EMGLProjectionViewMatrices):
 	def get_img_num_offset(self):
 		return self.img_num_offset
 
-	def set_draw_background(self,bool):
-		self.draw_background = bool
+	def set_draw_background(self, flag):
+		self.draw_background = flag
 		self.force_display_update()# empty display lists causes an automatic regeneration of the display list
 
-	def set_use_display_list(self,bool):
-		self.use_display_list = bool
+	def set_use_display_list(self, flag):
+		self.use_display_list = flag
 
 	def set_max_idx(self,n):
 		self.force_display_update()# empty display lists causes an automatic regeneration of the display list
@@ -2359,8 +2359,8 @@ class EMImageInspectorMX(QtGui.QWidget):
 			self.xyz.deleteLater()
 			self.xyz = None
 
-	def animation_clicked(self,bool):
-		self.target().animation_enabled = bool
+	def animation_clicked(self, flag):
+		self.target().animation_enabled = flag
 
 	def get_image_manip_page(self):
 		self.impage = QtGui.QWidget()
